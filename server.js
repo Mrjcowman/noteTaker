43c8bb37bf5887ab10c1e0b0a3faf02f159c5f42
Jacob Cowan <mrjcowman@gmail.com>
2020-10-07 17:52:15 -0700

    Implement note get method

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ app.get("/notes", (req, res)=>{
     res.sendFile(__dirname+"/public/notes.html");
 });
 
+// Responds with the json of notes from db.json
 app.get("/api/notes", (req, res)=>{
-    // TODO: implement note get
     console.log("Getting notes!");
+    fs.readFile(__dirname+"/db/db.json", (err, data)=>{
+        if(err) throw err;
+        console.log(JSON.parse(data));
+        res.send(JSON.parse(data));
+    })
 });
 
 app.get("*", (req, res)=>{
